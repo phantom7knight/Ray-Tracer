8dac0b3edba0513f0ee9a945ba22b3adbdbeb8bc
rohit.tolety <32849841+phantom7knight@users.noreply.github.com>
2019-01-19 02:13:34 -0800

    Made Ray class

--- Ray-Tracer/Ray-Tracer/Source/Ray.cpp
+++ Ray-Tracer/Ray-Tracer/Source/Ray.cpp
@@ -1,6 +1,12 @@
 #include "../pch.h"
 #include "../Source/Ray.h"
 
-Ray::Ray()
+
+
+Ray::Ray(const glm::vec3 & origin, const glm::vec3 & direction)
 {
+	m_rayOrigin		= origin;
+	m_rayDir		= direction;
+
+
 }
--- Ray-Tracer/Ray-Tracer/Source/Ray.h
+++ Ray-Tracer/Ray-Tracer/Source/Ray.h
@@ -1,15 +1,30 @@
 class Ray
 {
 public:
-	Ray();
+	Ray() {}
+
+	Ray(const glm::vec3& origin, const glm::vec3& direction);
 	
 	~Ray()
 	{
 	}
 
+	inline glm::vec3 getOrigin();
+	inline glm::vec3 getDirection();
 private:
 
-	glm::vec3 ray_dir;
-	glm::vec3 ray_start;
+	glm::vec3 m_rayDir;
+	glm::vec3 m_rayOrigin;
 
 };
+
+
+inline glm::vec3 Ray::getOrigin()
+{
+	return m_rayOrigin;
+}
+
+inline glm::vec3 Ray::getDirection()
+{
+	return m_rayDir;
+}
